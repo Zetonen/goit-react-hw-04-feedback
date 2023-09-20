3bcc77c9d9e9720e6890dda1031edd6868d3bc48
Seven <graf.lancelot7@gmail.com>
2023-09-20 20:56:13 +0300

    fix bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Section } from './App.styled';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
